33c22533328cf4cdeaa6fede37f084bb11bda6bd
Aldie NightStar <aldie.nightstar@gmail.com>
2023-05-06 19:01:50 +0200

    Small space fix

--- src/main/kotlin/haxidenti/flower/types/Util.kt
+++ src/main/kotlin/haxidenti/flower/types/Util.kt
@@ -3,7 +3,7 @@ package haxidenti.flower.types
 fun getReturnType(code: List<Instruction>) =
     code.firstNotNullOfOrNull { it as? ReturnInstruction }?.let { it.type } ?: Primitives.VOID
 
-fun  Instruction.required(type: VarType) {
+fun Instruction.required(type: VarType) {
     if (this.type != type) {
         throw TypedException(info, "Required type is ${type.name} but ${this.type.name} given for")
     }
